36ee6de948e354be51e5f48d594961621254a900
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-03-17 16:09:01 -0400

    CompFlow

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import './App.css';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-
-      </header>
+      <BrowserRouter>
+      <div>
+        <Link to ="/login">Login</Link>
+      </div>
+      <Switch>
+        <PrivateRoute path="/protected" component={FriendsList} />
+        <Route path="/login" component={Login} />
+        <Route component={Login} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
